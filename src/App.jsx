387728bf670ff27d264fc309f59d68657e1bf1c4
Elisha Adeboye <y.adeboye@gmail.com>
2023-09-12 13:21:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import Header from './components/Header/Header'
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <Header />
+        <p>Hello world</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logomov from '../../assets/logomov.png'
+import menu from '../../assets/menu.png'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header__container'>
+        <div className='logo__div'>
+            <img src={logomov} alt="logo image" className='logo__img'/>
+            <p>Movie Box</p>
+        </div>
+
+        <div class="search__block">
+          <span class="fa fa-search"></span>
+          <input type="search" placeholder="What do you want to watch?" />
+        </div>
+
+        <div className='auth__block'>
+          <p>Sign in</p>
+          <img src={menu} alt="mobilenav" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
