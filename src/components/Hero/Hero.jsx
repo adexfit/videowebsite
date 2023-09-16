4137d4be177f68a0b09c6b24e2b7735060a14cad
Elisha Adeboye <y.adeboye@gmail.com>
2023-09-16 07:23:37 +0100

    fixed hero error

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../Header/Header'
 import './Hero.css'
 import Play from '../../assets/Play.png'
-import Imbd from '../../assets/Imbd.png'
+import imbd from '../../assets/imbd.png'
 import apple from '../../assets/apple.png'
 
 
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className='hero__text'>
             <h1>John Wick 3: Parabellum</h1>
             <div className='label'>
-              <p><img src={Imbd} alt="imbd image" /> 86.0/100</p>
+              <p><img src={imbd} alt="imbd image" /> 86.0/100</p>
               <p><img src={apple} alt="apple image" />97%</p>
             </div>
             <p>John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.
