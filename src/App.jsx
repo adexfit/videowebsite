99a68aa4912366324c8418d177e37c97642997e2
Elisha Adeboye <y.adeboye@gmail.com>
2023-09-16 09:09:16 +0100

    fixed some errors

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   const [videoData, setvideoData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  // const [isLoading, setIsLoading] = useState(false)
 
 
   useEffect(() => {  
-    setIsLoading(true)
+    // setIsLoading(true)
 
     fetch("https://api.themoviedb.org/3/movie/top_rated/?api_key=21d1982e9f358e5f3273a53ce4d65108")
         .then(res => res.json())
         .then(data => setvideoData(data.results))
-        setIsLoading(false)
+        // setIsLoading(false)
     }, [])
 
 
@@ -25,7 +25,7 @@ function App() {
       {/* <BrowserRouter
       basename={import.meta.env.DEV ? '/' : '/videowebsite/'}>      */}
         <Routes>
-          <Route path='/' element={ <FeaturedMov videoData={videoData} isLoading={isLoading} /> } />
+          <Route path='/' element={ <FeaturedMov videoData={videoData} /> } />
           <Route path='/movies/:id' element={ <Dashboard /> } /> 
         </Routes>
       {/* </BrowserRouter>  */}
